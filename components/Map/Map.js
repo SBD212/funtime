b0369a46f2ce1dbf675b85ce7a77e7cbdc3c1e1c
Junaid Awan <awanj@cardiff.ac.uk>
2022-09-21 01:52:28 +0100

    deleted dummy.js

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -1,6 +1,8 @@
 import { useMemo } from "react";
 import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Map.module.css";
+import { Fragment } from "react";
 
 const Map = (props) => {
   const center = useMemo(() => ({ lat: 51.481583, lng: -3.17909 }), []);
@@ -9,28 +11,39 @@ const Map = (props) => {
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
   });
 
+  const [map, setMap] = useState(null);
+
   if (!isLoaded) {
     return <div>Loading Google Maps...</div>;
   }
 
   return (
-    <GoogleMap
-      center={center}
-      zoom={13}
-      mapContainerClassName={styles.map_container}
-      options={{
-        fullscreenControl: false,
-        streetViewControl: false,
-        mapTypeControl: false,
-      }}
-    >
-      {props.places.map((place) => (
-        <MarkerF
-          key={place.name}
-          position={{ lat: place.location.lat, lng: place.location.lng }}
+    <Fragment>
+      <button onClick={() => map.panTo(center)}>
+        <FontAwesomeIcon
+          icon="fa-solid fa-location-arrow"
+          style={{ height: "0.25rem" }}
         />
-      ))}
-    </GoogleMap>
+      </button>
+      <GoogleMap
+        center={center}
+        zoom={13}
+        mapContainerClassName={styles.map_container}
+        options={{
+          fullscreenControl: false,
+          streetViewControl: false,
+          mapTypeControl: false,
+        }}
+        onLoad={(map) => setMap(map)}
+      >
+        {props.places.map((place) => (
+          <MarkerF
+            key={place.name}
+            position={{ lat: place.location.lat, lng: place.location.lng }}
+          />
+        ))}
+      </GoogleMap>
+    </Fragment>
   );
 };
 
